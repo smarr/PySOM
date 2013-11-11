fd3578fe1af697486d66f33653abfec069079aeb
Stefan Marr <git@stefan-marr.de>
2013-11-11 18:20:02 +0100

    Change som.sh to prefer pypy as default Python
    Signed-off-by: Stefan Marr <git@stefan-marr.de>

--- som.sh
+++ som.sh
@@ -1,2 +1,8 @@
 #!/bin/sh
-PYTHONPATH=src python src/som/vm/universe.py "$@"
+DIR="`dirname \"$0\"`"
+if [ -z "$PYTHON" ]; then
+  PYTHON=pypy
+fi
+export PYTHONPATH=$DIR/src:$PYTHONPATH
+exec $PYTHON $DIR/src/som/vm/universe.py "$@"
+
