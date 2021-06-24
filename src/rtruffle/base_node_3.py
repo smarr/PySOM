8d44fd62990996dd207d13101fdab03d7d70a670
Stefan Marr <git@stefan-marr.de>
2021-06-24 23:24:23 +0100

    Apply black formatting and pylint fixes to rtruffle
    Signed-off-by: Stefan Marr <git@stefan-marr.de>

--- src/rtruffle/base_node_3.py
+++ src/rtruffle/base_node_3.py
@@ -2,5 +2,5 @@ from rtruffle.abstract_node import AbstractNode, NodeInitializeMetaClass
 
 
 class BaseNode(AbstractNode, metaclass=NodeInitializeMetaClass):
-    _immutable_fields_ = ['source_section', 'parent']
+    _immutable_fields_ = ["source_section", "parent"]
     _child_nodes_ = []
--- src/rtruffle/node.py
+++ src/rtruffle/node.py
@@ -1,6 +1,6 @@
-from rtruffle.source_section import SourceSection
-
 import sys
+
+from rtruffle.source_section import SourceSection
 
 if sys.version_info.major > 2:
     from rtruffle.base_node_3 import BaseNode
@@ -9,12 +9,10 @@ else:
 
 
 class Node(BaseNode):
-
-    def __init__(self, source_section = None):
-        assert (source_section is None or
-                isinstance(source_section, SourceSection))
+    def __init__(self, source_section=None):
+        assert source_section is None or isinstance(source_section, SourceSection)
         self.source_section = source_section
-        self.parent         = None
+        self.parent = None
 
     def assign_source_section(self, source_section):
         assert isinstance(source_section, SourceSection)
@@ -33,10 +31,10 @@ class Node(BaseNode):
             return None
 
         children = nodes[:]  # we return here a copy to make it clear to RPython
-                             # that the list is not resized, and,
-                             # the quasi-immutable support does not work on
-                             # element-level, so, we will need to copy the lists
-                             # when replacing child nodes
+        # that the list is not resized, and,
+        # the quasi-immutable support does not work on
+        # element-level, so, we will need to copy the lists
+        # when replacing child nodes
 
         for child in children:
             child.parent = self
@@ -44,11 +42,10 @@ class Node(BaseNode):
 
     def replace(self, node):
         if node:
-            self.parent._replace_child_with(self, node)
+            self.parent.replace_child_with(self, node)
             node.parent = self.parent
             return node
-        else:
-            return None
+        return None
 
     def __str__(self):
         return "%s(%s)" % (self.__class__.__name__, self.source_section)
