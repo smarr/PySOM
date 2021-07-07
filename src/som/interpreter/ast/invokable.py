ce87af7fb2fd4deeab8897513dab8c910082460a
Stefan Marr <git@stefan-marr.de>
2021-07-08 00:30:12 +0100

    Move the jit_merge_point to AstMethod
    I think it may be better to start the trace before creating a frame, because then we see the frame creation inside the trace.
    
    Signed-off-by: Stefan Marr <git@stefan-marr.de>

--- src/som/interpreter/ast/invokable.py
+++ src/som/interpreter/ast/invokable.py
@@ -1,29 +1,7 @@
-from rlib import jit
 from rlib.debug import make_sure_not_resized
 from rtruffle.node import Node
 
 from som.interpreter.ast.frame import create_frame
-
-
-def get_printable_location(invokable):
-    return invokable.source_section.identifier
-
-
-jitdriver = jit.JitDriver(
-    greens=["self"],
-    # virtualizables=["frame"],
-    get_printable_location=get_printable_location,
-    reds=["arguments", "receiver", "frame"],
-    is_recursive=True,
-    # the next line is a workaround around a likely bug in RPython
-    # for some reason, the inlining heuristics default to "never inline" when
-    # two different jit drivers are involved (in our case, the primitive
-    # driver, and this one).
-    # the next line says that calls involving this jitdriver should always be
-    # inlined once (which means that things like Integer>>< will be inlined
-    # into a while loop again, when enabling this driver).
-    should_unroll_one_iteration=lambda self: True,
-)
 
 
 class Invokable(Node):
@@ -54,9 +32,6 @@ class Invokable(Node):
         self._size_inner = size_inner
 
     def invoke(self, receiver, arguments):
-        assert arguments is not None
-        make_sure_not_resized(arguments)
-
         frame = create_frame(
             receiver,
             arguments,
@@ -64,8 +39,4 @@ class Invokable(Node):
             self._size_frame,
             self._size_inner,
         )
-        jitdriver.jit_merge_point(
-            self=self, receiver=receiver, arguments=arguments, frame=frame
-        )
-
         return self._expr_or_sequence.execute(frame)
--- src/som/vmobjects/method_ast.py
+++ src/som/vmobjects/method_ast.py
@@ -1,8 +1,30 @@
 from __future__ import absolute_import
 
 from rlib import jit
+from rlib.debug import make_sure_not_resized
 
 from som.vmobjects.method import AbstractMethod
+
+
+def get_printable_location(self):
+    return self._invokable.source_section.identifier
+
+
+jitdriver = jit.JitDriver(
+    greens=["self"],
+    # virtualizables=["frame"],
+    get_printable_location=get_printable_location,
+    reds=["arguments", "receiver"],
+    is_recursive=True,
+    # the next line is a workaround around a likely bug in RPython
+    # for some reason, the inlining heuristics default to "never inline" when
+    # two different jit drivers are involved (in our case, the primitive
+    # driver, and this one).
+    # the next line says that calls involving this jitdriver should always be
+    # inlined once (which means that things like Integer>>< will be inlined
+    # into a while loop again, when enabling this driver).
+    should_unroll_one_iteration=lambda self: True,
+)
 
 
 class AstMethod(AbstractMethod):
@@ -27,4 +49,9 @@ class AstMethod(AbstractMethod):
         return self.get_signature().get_number_of_signature_arguments()
 
     def invoke(self, receiver, args):
+        assert args is not None
+        make_sure_not_resized(args)
+
+        jitdriver.jit_merge_point(self=self, receiver=receiver, arguments=args)
+
         return self._invokable.invoke(receiver, args)
