bffa9b6da5f8a3e66beb4e40d99e8fefa21c3cd7
Stefan Marr <git@stefan-marr.de>
2021-07-15 16:17:53 +0100

    Added ifTrue/ifFalse primitives
    Signed-off-by: Stefan Marr <git@stefan-marr.de>

--- src/som/primitives/true_primitives.py
+++ src/som/primitives/true_primitives.py
@@ -1,7 +1,7 @@
 from som.interp_type import is_ast_interpreter
 from som.primitives.primitives import Primitives
-from som.vm.globals import trueObject, falseObject
-from som.vmobjects.primitive import UnaryPrimitive, BinaryPrimitive
+from som.vm.globals import trueObject, falseObject, nilObject
+from som.vmobjects.primitive import UnaryPrimitive, BinaryPrimitive, TernaryPrimitive
 
 if is_ast_interpreter():
     from som.vmobjects.block_ast import AstBlock as _Block
@@ -17,11 +17,22 @@ def _or(_rcvr, _arg):
     return trueObject
 
 
-def _and(_rcvr, arg):
+def _and_and_if_true(_rcvr, arg):
     if isinstance(arg, _Block):
         block_method = arg.get_method()
         return block_method.invoke_1(arg)
     return arg
+
+
+def _if_false(_rcvr, _arg):
+    return nilObject
+
+
+def _if_true_if_false(_rcvr, true_block, _false_block):
+    if isinstance(true_block, _Block):
+        block_method = true_block.get_method()
+        return block_method.invoke_1(true_block)
+    return true_block
 
 
 class TruePrimitivesBase(Primitives):
@@ -30,5 +41,19 @@ class TruePrimitivesBase(Primitives):
         self._install_instance_primitive(BinaryPrimitive("or:", self.universe, _or))
         self._install_instance_primitive(BinaryPrimitive("||", self.universe, _or))
 
-        self._install_instance_primitive(BinaryPrimitive("and:", self.universe, _and))
-        self._install_instance_primitive(BinaryPrimitive("&&", self.universe, _and))
+        self._install_instance_primitive(
+            BinaryPrimitive("and:", self.universe, _and_and_if_true)
+        )
+        self._install_instance_primitive(
+            BinaryPrimitive("&&", self.universe, _and_and_if_true)
+        )
+        self._install_instance_primitive(
+            BinaryPrimitive("ifTrue:", self.universe, _and_and_if_true)
+        )
+        self._install_instance_primitive(
+            BinaryPrimitive("ifFalse:", self.universe, _if_false)
+        )
+
+        self._install_instance_primitive(
+            TernaryPrimitive("ifTrue:ifFalse:", self.universe, _if_true_if_false)
+        )
