ca7e929067571c9dbc3c0e92f671070e292c6d60
Stefan Marr <git@stefan-marr.de>
2021-08-24 11:49:11 +0100

    Add negative tests for optimizing trivial methods
    Signed-off-by: Stefan Marr <git@stefan-marr.de>

--- tests/test_optimize_trivial.py
+++ tests/test_optimize_trivial.py
@@ -93,6 +93,12 @@ def test_global_return(mgenc, source):
     assert isinstance(m, GlobalRead)
 
 
+def test_non_trivial_global_return(mgenc):
+    body_or_none = parse_method(mgenc, "test = ( #foo. ^ system )")
+    m = mgenc.assemble(body_or_none)
+    assert isinstance(m, AstMethod) or isinstance(m, BcMethod)
+
+
 def test_field_getter_0(cgenc, mgenc):
     add_field(cgenc, "field")
     body_or_none = parse_method(mgenc, "test = ( ^ field )")
@@ -110,6 +116,25 @@ def test_field_getter_n(cgenc, mgenc):
     body_or_none = parse_method(mgenc, "test = ( ^ field )")
     m = mgenc.assemble(body_or_none)
     assert isinstance(m, FieldRead)
+
+
+def test_non_trivial_getter_0(cgenc, mgenc):
+    add_field(cgenc, "field")
+    body = parse_method(mgenc, "test = ( 0. ^ field )")
+    m = mgenc.assemble(body)
+    assert isinstance(m, AstMethod) or isinstance(m, BcMethod)
+
+
+def test_non_trivial_getter_n(cgenc, mgenc):
+    add_field(cgenc, "a")
+    add_field(cgenc, "b")
+    add_field(cgenc, "c")
+    add_field(cgenc, "d")
+    add_field(cgenc, "e")
+    add_field(cgenc, "field")
+    body = parse_method(mgenc, "test = ( 0. ^ field )")
+    m = mgenc.assemble(body)
+    assert isinstance(m, AstMethod) or isinstance(m, BcMethod)
 
 
 @pytest.mark.parametrize(
@@ -137,6 +162,25 @@ def test_field_setter_n(cgenc, mgenc, source):
     assert isinstance(m, FieldWrite)
 
 
+def test_non_trivial_field_setter_0(cgenc, mgenc):
+    add_field(cgenc, "field")
+    body_or_none = parse_method(mgenc, "test: val = ( 0. field := value )")
+    m = mgenc.assemble(body_or_none)
+    assert isinstance(m, AstMethod) or isinstance(m, BcMethod)
+
+
+def test_non_trivial_field_setter_n(cgenc, mgenc):
+    add_field(cgenc, "a")
+    add_field(cgenc, "b")
+    add_field(cgenc, "c")
+    add_field(cgenc, "d")
+    add_field(cgenc, "e")
+    add_field(cgenc, "field")
+    body_or_none = parse_method(mgenc, "test: val = ( 0. field := value )")
+    m = mgenc.assemble(body_or_none)
+    assert isinstance(m, AstMethod) or isinstance(m, BcMethod)
+
+
 @pytest.mark.parametrize(
     "source",
     [
@@ -154,6 +198,27 @@ def test_field_setter_n(cgenc, mgenc, source):
 )
 def test_literal_no_return(mgenc, source):
     body_or_none = parse_method(mgenc, "test = ( " + source + " )")
+    m = mgenc.assemble(body_or_none)
+    assert isinstance(m, AstMethod) or isinstance(m, BcMethod)
+
+
+@pytest.mark.parametrize(
+    "source",
+    [
+        "0",
+        "1",
+        "-10",
+        "'str'",
+        "#sym",
+        "1.1",
+        "-2342.234",
+        "true",
+        "false",
+        "nil",
+    ],
+)
+def test_non_trivial_literal_return(mgenc, source):
+    body_or_none = parse_method(mgenc, "test = ( 1. ^ " + source + " )")
     m = mgenc.assemble(body_or_none)
     assert isinstance(m, AstMethod) or isinstance(m, BcMethod)
 
@@ -185,3 +250,13 @@ def test_block_literal_return(bgenc, source, test_result):
     m = bgenc.assemble(body_or_none)
     assert isinstance(m, LiteralReturn)
     assert str(m.invoke_1(None)) == test_result
+
+
+def test_unknown_global_in_block(bgenc):
+    """
+    In PySOM we can actually support this, in TruffleSOM we can't
+    because of the difference in frame format.
+    """
+    body_or_none = parse_block(bgenc, "[ UnknownGlobalSSSS ]")
+    m = bgenc.assemble(body_or_none)
+    assert isinstance(m, GlobalRead)
