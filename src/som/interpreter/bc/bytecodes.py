bbcb63ea58660ee68810e7c30e51c93565782758
Stefan Marr <git@stefan-marr.de>
2021-08-30 18:44:38 +0100

    Remove unused code
    Signed-off-by: Stefan Marr <git@stefan-marr.de>

--- src/som/interpreter/bc/bytecodes.py
+++ src/som/interpreter/bc/bytecodes.py
@@ -270,101 +270,11 @@ _BYTECODE_LENGTH = [
     3,  # pop_argument
 ]
 
-# chose a unreasonable number to be recognizable
-_STACK_EFFECT_DEPENDS_ON_MESSAGE = -1000
-
-_BYTECODE_STACK_EFFECT = [
-    0,  # halt
-    1,  # dup
-    1,  # push_frame
-    1,  # push_frame_0
-    1,  # push_frame_1
-    1,  # push_frame_2
-    1,  # push_inner
-    1,  # push_inner_0
-    1,  # push_inner_1
-    1,  # push_inner_2
-    1,  # push_field
-    1,  # push_field_0
-    1,  # push_field_1
-    1,  # push_block
-    1,  # push_block_no_ctx
-    1,  # push_constant
-    1,  # push_constant_0
-    1,  # push_constant_1
-    1,  # push_constant_2
-    1,  # push_0
-    1,  # push_1
-    1,  # push_nil
-    1,  # push_global
-    -1,  # pop
-    -1,  # pop_frame
-    -1,  # pop_frame_0
-    -1,  # pop_frame_1
-    -1,  # pop_frame_2
-    -1,  # pop_inner
-    -1,  # pop_inner_0
-    -1,  # pop_inner_1
-    -1,  # pop_inner_2
-    -1,  # pop_field
-    -1,  # pop_field_0
-    -1,  # pop_field_1
-    _STACK_EFFECT_DEPENDS_ON_MESSAGE,  # send_1
-    _STACK_EFFECT_DEPENDS_ON_MESSAGE,  # send_2
-    _STACK_EFFECT_DEPENDS_ON_MESSAGE,  # send_3
-    _STACK_EFFECT_DEPENDS_ON_MESSAGE,  # send_n
-    _STACK_EFFECT_DEPENDS_ON_MESSAGE,  # super_send
-    0,  # return_local
-    0,  # return_non_local
-    0,  # return_self
-    0,  # return_field_0
-    0,  # return_field_1
-    0,  # return_field_2
-    0,  # inc
-    0,  # dec
-    0,  # inc_field
-    1,  # inc_field_push
-    0,  # jump
-    0,  # jump_on_true_top_nil
-    0,  # jump_on_false_top_nil
-    -1,  # jump_on_true_pop
-    -1,  # jump_on_false_pop
-    0,  # jump_backward
-    0,  # jump2
-    0,  # jump2_on_true_top_nil
-    0,  # jump2_on_false_top_nil
-    -1,  # jump2_on_true_pop
-    -1,  # jump2_on_false_pop
-    0,  # jump2_backward
-    _STACK_EFFECT_DEPENDS_ON_MESSAGE,  # q_super_send_1
-    _STACK_EFFECT_DEPENDS_ON_MESSAGE,  # q_super_send_2
-    _STACK_EFFECT_DEPENDS_ON_MESSAGE,  # q_super_send_3
-    _STACK_EFFECT_DEPENDS_ON_MESSAGE,  # q_super_send_n
-    1,  # push_argument
-    1,  # push_field
-    -1,  # pop_local
-    -1,  # pop_argument
-]
-
 
 @jit.elidable
 def bytecode_length(bytecode):
     assert 0 <= bytecode < len(_BYTECODE_LENGTH)
     return _BYTECODE_LENGTH[bytecode]
-
-
-@jit.elidable
-def bytecode_stack_effect(bytecode, number_of_arguments_of_message_send=0):
-    assert 0 <= bytecode < len(_BYTECODE_STACK_EFFECT)
-    if bytecode_stack_effect_depends_on_send(bytecode):
-        # +1 in order to account for the return value
-        return -number_of_arguments_of_message_send + 1
-    return _BYTECODE_STACK_EFFECT[bytecode]
-
-
-def bytecode_stack_effect_depends_on_send(bytecode):
-    assert 0 <= bytecode < len(_BYTECODE_STACK_EFFECT)
-    return _BYTECODE_STACK_EFFECT[bytecode] == _STACK_EFFECT_DEPENDS_ON_MESSAGE
 
 
 @jit.elidable
